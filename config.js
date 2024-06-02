fe24ae373de552a105e1584e2d2b4eaea2343fbd
Rachel Johnson <rachelktyjohnson@gmail.com>
2024-06-02 14:17:19 +1000

    Initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                rose_white: 'hsl(330, 100%, 98%)',
+                eggshell: 'hsl(30, 54%, 90%)',
+                light_grey: 'hsl(30, 18%, 87%)',
+                wenge_brown: 'hsl(30, 10%, 34%)',
+                brown: '#8B513E',
+                dark_charcoal: 'hsl(24, 5%, 18%)'
+            },
+            fontFamily: {
+                sans: ['Outfit', 'sans-serif'],
+                serif: ['Young Serif', 'serif']
+            }
+        }
+    }
+}
